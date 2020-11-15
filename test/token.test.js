cd610d1463284e038f4183801659f8530192591d
Raphael Tomé Santana <raphaelts3@gmail.com>
2020-11-15 20:26:34 -0300

    Fix test

--- test/token.test.js
+++ test/token.test.js
@@ -1,10 +1,11 @@
-require("dotenv").config();
 const assert = require("assert");
 const jwt = require("jsonwebtoken");
 
 describe("Token", function () {
   describe("#generator", function () {
     it("should generate a valid token", function () {
+      process.env.MARVELL_SECRET = "secret";
+      process.env.TOKEN_TIMEOUT = 60;
       const hashed = require("../cli/token");
 
       jwt.verify(
