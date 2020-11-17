ece6d926dc8abf8081ab930d1d8b042b64c5b6f5
Raphael Tomé Santana <raphaelts3@gmail.com>
2020-11-17 19:10:24 -0300

    Changing from Eres.js to discord.js

--- discord/index.js
+++ discord/index.js
@@ -1,42 +1,41 @@
 require("dotenv").config();
 const token = require("../common/token");
-const Eris = require("eris");
+const Discord = require("discord.js");
+const bot = new Discord.Client();
 
-const bot = new Eris.CommandClient(
-  process.env.DISCORD_BOT_TOKEN,
-  {},
-  {
-    description:
-      "A bot to generate a link to authorize Mar-Vell bot in your channel.",
-    owner: "raphaelts3",
-    prefix: "!",
-  }
-);
+const prefix = "!";
 
 bot.on("ready", () => {
   // When the bot is ready
-  console.log("Bot Ready!"); // Log "Ready!"
+  console.log("Bot Ready!");
 });
 
-const echoCommand = bot.registerCommand(
-  "generate-token",
-  (msg, args) => {
-    // Make an echo command
-    console.log(msg, args);
-    // Check if user has "Porteiro" role at "Mar-Vell Bot Discord"
-    if (!msg.member.roles.some((role) => role === "778319142281019412")) {
-      return `<@${msg.member.user.id}> here you go https://bit.ly/35Crtkf`;
-    }
-    // Return message with token
-    const _token = token.generate();
-    return `<@${msg.member.user.id}> here you go ${process.env.BASE_URI}/auth/twitch/${_token}`;
-  },
-  {
-    description: "Generate token for Mar-Vell Bot",
-    fullDescription:
-      "The bot will generate a link to authorize Mar-Vell bot in your channel.",
-    usage: "",
+const generateToken = (channel, member) => {
+  // User must have the role "MarVellHelper"
+  if (!member.roles.cache.some((role) => role.name === "MarVellHelper")) {
+    channel.send(
+      `<@${member.user.id}> here you go https://bit.ly/35Crtkf`
+    );
+    return;
   }
-);
+  // Return message with token
+  const _token = token.generate();
+  channel.send(
+    `<@${member.user.id}> here you go ${process.env.BASE_URI}/auth/twitch/${_token}`
+  );
+};
 
-bot.connect(); // Get the bot to connect to Discord
+// Handler for messages
+bot.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  // Split command and arguments
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command == "generate-tokex") {
+    return generateToken(message.channel, message.member);
+  }
+});
+
+bot.login(process.env.DISCORD_BOT_TOKEN); // Login bot
