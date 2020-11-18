386c87a03a0bcf390fd6ff3ef8130faf191a410e
Raphael Tomé Santana <raphaelts3@gmail.com>
2020-11-18 14:21:24 -0300

    Fixing subcription to webhooks and cli

--- test/token.test.js
+++ test/token.test.js
@@ -3,22 +3,38 @@ const jwt = require("jsonwebtoken");
 
 describe("Token", function () {
   describe("#generator", function () {
-    it("should generate a valid token", function () {
+    it("should generate a valid token", function (done) {
       process.env.MARVELL_SECRET = "secret";
       process.env.TOKEN_TIMEOUT = 60;
-      const hashed = require("../cli/token");
-
-      jwt.verify(
-        Buffer.from(hashed, "base64").toString(),
-        process.env.MARVELL_SECRET,
-        {
-          ignoreExpiration: false,
-          maxAge: Number.parseInt(process.env.TOKEN_TIMEOUT),
-        },
-        function (err, decoded) {
-          assert.strictEqual(err, null);
-        }
-      );
+      const tokenService = require("../common/token");
+      const token = tokenService.generate();
+      tokenService.verify(token, function (err, decoded) {
+        assert.strictEqual(err, null);
+        done();
+      });
+    });
+    it("token should be invalid", function (done) {
+      process.env.MARVELL_SECRET = "secret";
+      process.env.TOKEN_TIMEOUT = 60;
+      const tokenService = require("../common/token");
+      const token = tokenService.generate();
+      process.env.MARVELL_SECRET = "not-secret";
+      tokenService.verify(token, function (err, decoded) {
+        assert.strictEqual(err.name, "JsonWebTokenError");
+        done();
+      });
+    });
+    it("token should be expired", function (done) {
+      process.env.MARVELL_SECRET = "secret";
+      process.env.TOKEN_TIMEOUT = 1;
+      const tokenService = require("../common/token");
+      const token = tokenService.generate();
+      setTimeout(() => {
+        tokenService.verify(token, function (err, decoded) {
+          assert.strictEqual(err.name, "TokenExpiredError");
+          done();
+        });
+      }, 1000);
     });
   });
 });
